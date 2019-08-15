ab47b496ed65173956608c9d67f8640bbcc749e7
sam <smuoki@healthcloud.co.ke>
2019-08-15 20:18:25 +0300

    Setup circleci default settings

--- documents_tracer/documents_tracer/wsgi.py
+++ documents_tracer/documents_tracer/wsgi.py
@@ -12,5 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'documents_tracer.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'documents_tracer.config.settings')
+
 
 application = get_wsgi_application()
--- documents_tracer/id_cards/models.py
+++ documents_tracer/id_cards/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Lost_ID_card(models.model):
+    """
+    Model for lost national ids that will be uploaded
+    """
+    id_number = models.CharField(maxlength=15)
+    full_name = models.CharField(maxlength=15)
+    date_of_birth = models.DateField
+    sex = models.CharField(maxlength=15)
+    date_found = models.DateTimeField('Less than or equal to Today')
+    place_found = models.TextField
+    office_submitted = models.TextField
+    picture = models.FileField
+
+
+class Searched_id(models.model):
+    """
+    Model for searched ids
+    """
+    id_number = models.CharField(max_length=15)
+    full_name = models.CharField(max_length=50)
+    contact_number = models.CharField(maxlength=15)
+    contact_email = models.EmailField
